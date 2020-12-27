dcdb6d068e6221c7be8f44134f77c3831d156950
usyrle <awy6143@gmail.com>
2020-12-27 14:29:12 -0600

    init homepage with button and fan content policy

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="game-opts">
+        <button className="new-game">New Game</button>
+      </div>
+      <footer>
+        <div className="source-links">
+          <p><a href="https://github.com/usyrle/aethermap/">Aethermap source code</a></p>
+          <p><a href="https://github.com/usyrle/aetherstream/">Aetherstream (API) source code</a></p>
+        </div>
+        <div className="fan-policy">
+          <p>Aethermap is unofficial Fan Content permitted under the Fan Content Policy. Not approved/endorsed by Wizards.</p>
+          <p>Portions of the materials used are property of Wizards of the Coast.</p>
+          <p>©Wizards of the Coast LLC.</p>
+        </div>
+      </footer>
     </div>
   );
 }
