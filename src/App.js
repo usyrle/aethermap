ba32c0153925b2c4b334e98b4fa2113a8482a655
usyrle <awy6143@gmail.com>
2020-12-28 11:21:48 -0600

    add transition to new game id based on api response

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import axios from 'axios';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useParams
+} from "react-router-dom";
+import {
+  RouteTransitionProvider,
+  useTransitionHistory
+} from 'react-route-transition';
 import './App.css';
 
-function App() {
-  const requestNewDeck = () => {
-    axios.post("https://aetherstream.herokuapp.com/deck/generate", {
-      size: 10,
-      phenomena: true,
-    }).then((response) => {
-      console.log(response)
-    });
-  }
 
+function App() {
   return (
     <div className="App">
-      <div className="game-opts">
-        <button className="new-game" onClick={requestNewDeck}>New Game</button>
-      </div>
+      <Router>
+        <RouteTransitionProvider>
+          <Switch>
+            <Route path={`/:gameId`}>
+              <GameSession />
+            </Route>
+            <Route path="/">
+              <GameOpts />
+            </Route>
+          </Switch>
+        </RouteTransitionProvider>
+      </Router>
       <footer>
         <div className="source-links">
           <p><a href="https://github.com/usyrle/aethermap/">Aethermap source code</a></p>
@@ -29,7 +40,31 @@ function App() {
         </div>
       </footer>
     </div>
-  );
+  )
+}
+
+function GameOpts() {
+  const history = useTransitionHistory();
+
+  const requestNewDeck = () => {
+    axios.post("https://aetherstream.herokuapp.com/deck/generate", {
+      size: 10,
+      phenomena: true,
+    }).then((response) => {
+      history.push(response.data.id)
+    })
+  }
+
+  return (
+    <div className="game-opts">
+      <button className="new-game" onClick={requestNewDeck}>New Game</button>
+    </div>
+  )
+}
+
+function GameSession() {
+  let { gameId } = useParams();
+  return <h3>Requested topic ID: {gameId}</h3>;
 }
 
 export default App;
