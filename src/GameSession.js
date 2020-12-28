4df66567c0a7dda9f96135f4cc01d8741667e405
usyrle <awy6143@gmail.com>
2020-12-28 17:01:05 -0600

    add next button to get next plane

--- src/GameSession.js
+++ src/GameSession.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 import { useParams } from "react-router-dom"
+import { useTransitionHistory } from 'react-route-transition'
 import cardImages from './cards/images'
 
 import './GameSession.css'
@@ -8,18 +9,38 @@ import './GameSession.css'
 export function GameSession() {
     const { gameId } = useParams()
     const [cardImgSrc, setCardImgSrc] = useState("")
+    const history = useTransitionHistory()
 
-    axios.get(`https://aetherstream.herokuapp.com/deck/${gameId}`)
+    const gameUrl = `https://aetherstream.herokuapp.com/deck/${gameId}`
+
+    const goHome = () => {
+        history.push('/')
+    }
+
+    const getNext = () => {
+        setCardImgSrc(getNextPlane(gameUrl))
+    }
+
+    axios.get(gameUrl)
         .then((response) => {
-            console.log(response);
             let cardId = response.data.currentPlane.multiverseId;
-            console.log(cardImages[cardId].default);
             setCardImgSrc(cardImages[cardId].default);
         })
 
     return (
-        <div class="plane-card">
-            <img src={cardImgSrc} alt="current plane" />
+        <div className="game-session">
+            <div className="button-row">
+                <button className="home-button" onClick={goHome}>Home</button>
+                <button className="next-button" onClick={getNext}>Next Plane</button>
+            </div>
+            <div className="plane-card">
+                <img src={cardImgSrc} alt="current plane" />
+            </div>
         </div>
     )
+}
+
+async function getNextPlane(url) {
+    let response = await axios.post(`${url}/next`)
+    return response.data.currentPlane.multiverseId
 }
